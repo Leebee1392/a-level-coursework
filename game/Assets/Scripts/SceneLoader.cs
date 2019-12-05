7a69fe1c9d1ae17f686c03f4469499734b8f8f0a
Leanne Billington <leanne.billington@ymail.com>
2019-12-05 12:59:14 +0000

    Buttons now work

--- game/Assets/Scripts/SceneLoader.cs
+++ game/Assets/Scripts/SceneLoader.cs
@@ -9,6 +9,26 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadStoryScreen1()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadClassicScreen()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadEndlessRunnerScreen()
+    {
+        SceneManager.LoadScene(4);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
